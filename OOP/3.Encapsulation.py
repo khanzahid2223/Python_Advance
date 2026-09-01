a95fd539b0cc7ffb030cfef65705b3104cef3d61
Zahid <khanzahid7553@gmail.com>
2026-09-01 11:15:31 +0530

    first commit

---
+++ OOP/3.Encapsulation.py
@@ -0,0 +1,18 @@
+"""
+Create a BankAccount where balance cannot be directly modified.
+Create getter and setter methods for age.
+Create a Student class where marks must remain between 0 and 100.
+Create an Employee class with private salary.
+Prevent negative salary using encapsulation.
+Create a PasswordManager with a private password.
+Create a User class where email can only be changed through a method.
+Create a Product class where price cannot be negative.
+Create a Wallet class with private balance.
+Create a Person class where age cannot be negative.
+Create a BankAccount where withdrawal cannot exceed balance.
+Create a Mobile class with a private battery percentage.
+Create a LoginSystem with private username and password.
+Challenge: Design a secure ATM class using encapsulation
+"""
+
+#Create a class with a private variable.
