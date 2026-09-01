a90d10778a3f5d664a01c411130b49be538e848a
Zahid <khanzahid7553@gmail.com>
2026-09-01 11:15:54 +0530

    Modify

--- OOP/1.OOP Foundation.py
+++ OOP/1.OOP Foundation.py
@@ -1,189 +1,189 @@
-# #Create a Student class.
-# class Student:
-#     def __init__(self,name:str,age:int):
-#         self.name=name
-#         self.age=age
+#Create a Student class.
+class Student:
+    def __init__(self,name:str,age:int):
+        self.name=name
+        self.age=age
 
-#     def display(self):
-#         print(f"Student Name={self.name}")
-#         print(f"Student Age={self.age}")
+    def display(self):
+        print(f"Student Name={self.name}")
+        print(f"Student Age={self.age}")
 
-# S1=Student("Name",21)
-# S1.display()
+S1=Student("Name",21)
+S1.display()
 
-# #Create 3 objects of Student.
-# class Student:
-#     def __init__(self,name:str,age:int,gender:str):
-#          self.name=name
-#          self.age=age
-#          self.gender=gender
+#Create 3 objects of Student.
+class Student:
+    def __init__(self,name:str,age:int,gender:str):
+         self.name=name
+         self.age=age
+         self.gender=gender
 
-#     def display(self):
-#          print(f"Student Name:{self.name}")
-#          print(f"Student Age:{self.age}")
-#          print(f"Student Gender:{self.gender}")
-#          print("-"*20)
+    def display(self):
+         print(f"Student Name:{self.name}")
+         print(f"Student Age:{self.age}")
+         print(f"Student Gender:{self.gender}")
+         print("-"*20)
 
-# S1=Student("Zahid",21,"Male")
-# S2=Student("Arosh",22,"Female")
-# S3=Student("Furkhan",12,"Male")
-# S1.display()
-# S2.display()
-# S3.display()
-
-
-# # Create a Car class with brand, model and price.
-# class Car:
-#     def __init__(self, brand: str, model: str, price: int)-> None:
-#         self.brand = brand
-#         self.model = model
-#         self.price = price
-
-#     def display(self):
-#         print(f"Brand of a Car is:{self.brand}")
-#         print(f"Model of a Car is:{self.model}")
-#         print(f"Price of a Car is:{self.price}")
+S1=Student("Zahid",21,"Male")
+S2=Student("Arosh",22,"Female")
+S3=Student("Furkhan",12,"Male")
+S1.display()
+S2.display()
+S3.display()
 
 
-# C = Car("Benz", "Mercides", "2000000")
-# C.display()
+# Create a Car class with brand, model and price.
+class Car:
+    def __init__(self, brand: str, model: str, price: int)-> None:
+        self.brand = brand
+        self.model = model
+        self.price = price
 
-# #Create a Person class and initialize name and age.
-# class Person:
-#     def __init__(self,name:str,age:int):
-#         self.name=name
-#         self.age=age
+    def display(self):
+        print(f"Brand of a Car is:{self.brand}")
+        print(f"Model of a Car is:{self.model}")
+        print(f"Price of a Car is:{self.price}")
 
-#     def display(self):
-#         print(f"Name:{self.name}")
-#         print(f"Age:{self.age}")
 
-# P1=Person("Zahid",21)
-# P1.display()
+C = Car("Benz", "Mercides", "2000000")
+C.display()
 
-# #Create a Book class with title and author.
-# class Book:
-#     def __init__(self,title:str,author:str)->None:
-#         self.title=title
-#         self.author=author
+#Create a Person class and initialize name and age.
+class Person:
+    def __init__(self,name:str,age:int):
+        self.name=name
+        self.age=age
 
-#     def display(self):
-#         print(f"Title of a Book is:{self.title}")
-#         print(f"Author of the Book is:{self.author}")
+    def display(self):
+        print(f"Name:{self.name}")
+        print(f"Age:{self.age}")
 
-# B1=Book("Coding with Khan","Zahid Khan")
-# B1.display()
+P1=Person("Zahid",21)
+P1.display()
 
-# #Create a Rectangle class and calculate area.
-# class Rectangle:
-#     def __init__(self,l,b):
-#         self.l=l
-#         self.b=b
+#Create a Book class with title and author.
+class Book:
+    def __init__(self,title:str,author:str)->None:
+        self.title=title
+        self.author=author
 
-#     def display(self):
-#         print(f"Area of a Rectangle is:{self.l*self.b}")
+    def display(self):
+        print(f"Title of a Book is:{self.title}")
+        print(f"Author of the Book is:{self.author}")
 
-# R1=Rectangle(10,20)
-# R1.display()
+B1=Book("Coding with Khan","Zahid Khan")
+B1.display()
 
-# #Create a Circle class and calculate area.
-# class Circle:
-#     def __init__(self,r):
-#         self.r=r
+#Create a Rectangle class and calculate area.
+class Rectangle:
+    def __init__(self,l,b):
+        self.l=l
+        self.b=b
 
-#     def display(self):
-#         print(f"Area of a Circle is:{3.14*self.r**2:.0f}")
+    def display(self):
+        print(f"Area of a Rectangle is:{self.l*self.b}")
 
-# C1=Circle(3)
-# C1.display()
+R1=Rectangle(10,20)
+R1.display()
 
-# #Create a BankAccount class with account number and balance.
-# class BankAccount:
-#     def __init__(self,acc_number:int,balance:int):
-#         self.acc_number=acc_number
-#         self.balance=balance
+#Create a Circle class and calculate area.
+class Circle:
+    def __init__(self,r):
+        self.r=r
 
-#     def display(self):
-#         print(f"Account number:{self.acc_number}")
-#         print(f"Balance:{self.balance}")
+    def display(self):
+        print(f"Area of a Circle is:{3.14*self.r**2:.0f}")
 
-# B1=BankAccount(8147642073,50000)
-# B1.display()
+C1=Circle(3)
+C1.display()
 
-# #Create a Laptop class and display its details.
-# class Laptop:
-#     def __init__(self,RAM:int,SSD:str,Processor:str)->None:
-#         self.RAM=RAM
-#         self.SSD=SSD
-#         self.Processor=Processor
+#Create a BankAccount class with account number and balance.
+class BankAccount:
+    def __init__(self,acc_number:int,balance:int):
+        self.acc_number=acc_number
+        self.balance=balance
 
-#     def display(self):
-#         print(f"RAM of a Laptop is:{self.RAM}")
-#         print(f"SSD of a Laptop is:{self.SSD}")
-#         print(f"Processor of Laptop is:{self.Processor}")
+    def display(self):
+        print(f"Account number:{self.acc_number}")
+        print(f"Balance:{self.balance}")
 
-# L1=Laptop(12,"1TB","Intel")
-# L1.display()
+B1=BankAccount(8147642073,50000)
+B1.display()
 
-# #Create a Mobile class with brand, model and price.
-# class Mobile:
-#     def __init__(self,brand:str,model:str,price:int):
-#         self.brand=brand
-#         self.model=model
-#         self.price=price
+#Create a Laptop class and display its details.
+class Laptop:
+    def __init__(self,RAM:int,SSD:str,Processor:str)->None:
+        self.RAM=RAM
+        self.SSD=SSD
+        self.Processor=Processor
 
-#     def display(self):
-#         print(f"Mobile brand is {self.brand}, Mobile model is {self.model} and the price of a mobile is {self.price}")
+    def display(self):
+        print(f"RAM of a Laptop is:{self.RAM}")
+        print(f"SSD of a Laptop is:{self.SSD}")
+        print(f"Processor of Laptop is:{self.Processor}")
 
-# M1=Mobile("VIVO","Y28 5G",15000)
-# M1.display()
+L1=Laptop(12,"1TB","Intel")
+L1.display()
 
-# #Create a Student class with marks and calculate average.
-# class Student:
-#     def __init__(self,marks:list[int]):
-#         self.marks=marks
+#Create a Mobile class with brand, model and price.
+class Mobile:
+    def __init__(self,brand:str,model:str,price:int):
+        self.brand=brand
+        self.model=model
+        self.price=price
 
-#     def avg(self):
-#         total=sum(self.marks)
-#         avg=total/len(self.marks)
-#         print(f"Average:{avg:.0f}")
+    def display(self):
+        print(f"Mobile brand is {self.brand}, Mobile model is {self.model} and the price of a mobile is {self.price}")
 
-# S1=Student([90,98,80,98,48])
-# S1.avg()
+M1=Mobile("VIVO","Y28 5G",15000)
+M1.display()
 
-# #Create an Employee class with name and salary.
-# class Employe:
-#     def __init__(self,name:str,salary:int):
-#         self.name=name
-#         self.salary=salary
+#Create a Student class with marks and calculate average.
+class Student:
+    def __init__(self,marks:list[int]):
+        self.marks=marks
 
-#     def display(self):
-#         print(f"Employee name is {self.name} and the salary of that employee is {self.salary}")
+    def avg(self):
+        total=sum(self.marks)
+        avg=total/len(self.marks)
+        print(f"Average:{avg:.0f}")
 
-# E1=Employe("Zahid","100000")
-# E1.display()
+S1=Student([90,98,80,98,48])
+S1.avg()
 
-# #Create a Product class with name, price and quantity.
-# class Product:
-#     def __init__(self,name:str,price:str,quantity:int):
-#         self.name=name
-#         self.price=price
-#         self.quantity=quantity
+#Create an Employee class with name and salary.
+class Employe:
+    def __init__(self,name:str,salary:int):
+        self.name=name
+        self.salary=salary
 
-#     def display(self):
-#         print(f"Product name is {self.name}, price of the prodect is {self.price} and the quantity of the prodect is {self.quantity}")
+    def display(self):
+        print(f"Employee name is {self.name} and the salary of that employee is {self.salary}")
 
-# P1=Product("Mobile",20000,20)
-# P1.display()
+E1=Employe("Zahid","100000")
+E1.display()
 
-# #Create a Movie class with title, rating 
-# class Movie:
-#     def __init__(self,title:str,rating:int):
-#         self.title=title
-#         self.rating=rating
+#Create a Product class with name, price and quantity.
+class Product:
+    def __init__(self,name:str,price:str,quantity:int):
+        self.name=name
+        self.price=price
+        self.quantity=quantity
 
-#     def display(self):
-#         print(f"Title of the movie is {self.title} and the rating for that movie is {self.rating}")
+    def display(self):
+        print(f"Product name is {self.name}, price of the prodect is {self.price} and the quantity of the prodect is {self.quantity}")
 
-# M1=Movie("Pathan",4.5)
-# M1.display()
+P1=Product("Mobile",20000,20)
+P1.display()
+
+#Create a Movie class with title, rating 
+class Movie:
+    def __init__(self,title:str,rating:int):
+        self.title=title
+        self.rating=rating
+
+    def display(self):
+        print(f"Title of the movie is {self.title} and the rating for that movie is {self.rating}")
+
+M1=Movie("Pathan",4.5)
+M1.display()
--- OOP/2.Constructors & Methods.py
+++ OOP/2.Constructors & Methods.py
@@ -1,190 +1,190 @@
-# #Create a class using __init__().
-# class Sample:
-#     def __init__(self):
-#         print("Hello Developer")
+#Create a class using __init__().
+class Sample:
+    def __init__(self):
+        print("Hello Developer")
 
-# S1=Sample()
+S1=Sample()
 
-# #Create a class with multiple constructor parameters.
-# class Student:
-#     def __init__(self,name:str,age:int,gender:str):
-#         self.name=name
-#         self.age=age
-#         self.gender=gender
+#Create a class with multiple constructor parameters.
+class Student:
+    def __init__(self,name:str,age:int,gender:str):
+        self.name=name
+        self.age=age
+        self.gender=gender
 
-#     def display(self):
-#         print(f"My name is {self.name}, My age is {self.age} and My gender is {self.gender}")
+    def display(self):
+        print(f"My name is {self.name}, My age is {self.age} and My gender is {self.gender}")
 
-# S1=Student("Zahid",21,"Male")
-# S1.display()
+S1=Student("Zahid",21,"Male")
+S1.display()
 
-#Create a Student class with a method to display details.
-# class Student:
-#     def __init__(self,name:str,age:int,gender:str):
-#         self.name=name
-#         self.age=age
-#         self.gender=gender
+Create a Student class with a method to display details.
+class Student:
+    def __init__(self,name:str,age:int,gender:str):
+        self.name=name
+        self.age=age
+        self.gender=gender
 
-#     def display(self):
-#         print(f"My name is {self.name}, My age is {self.age} and My gender is {self.gender}")
+    def display(self):
+        print(f"My name is {self.name}, My age is {self.age} and My gender is {self.gender}")
 
-# S1=Student("Zahid",21,"Male")
-# S1.display()
+S1=Student("Zahid",21,"Male")
+S1.display()
 
-# #Create a Calculator class with add, subtract, multiply and divide methods.
-# class Calculator:
-#     def __init__(self,num1:int,num2:int)->None:
-#         self.num1=num1
-#         self.num2=num2
+#Create a Calculator class with add, subtract, multiply and divide methods.
+class Calculator:
+    def __init__(self,num1:int,num2:int)->None:
+        self.num1=num1
+        self.num2=num2
 
-#     def add(self):
-#         print(f"Addition:{self.num1+self.num2}")
+    def add(self):
+        print(f"Addition:{self.num1+self.num2}")
 
-#     def sub(self):
-#         print(f"Subtraction:{self.num1-self.num2}")
+    def sub(self):
+        print(f"Subtraction:{self.num1-self.num2}")
 
-#     def mul(self):
-#         print(f"Multiplication:{self.num1*self.num2}")
+    def mul(self):
+        print(f"Multiplication:{self.num1*self.num2}")
 
-#     def div(self):
-#         print(f"Division:{self.num1%self.num2}")
+    def div(self):
+        print(f"Division:{self.num1%self.num2}")
 
-# Cal=Calculator(100,50)
-# Cal.add()
-# Cal.sub()
-# Cal.mul()
-# Cal.div()
+Cal=Calculator(100,50)
+Cal.add()
+Cal.sub()
+Cal.mul()
+Cal.div()
 
-# #Create a BankAccount with deposit and withdraw methods
-# class BankAccount:
-#     Balance=100000
-#     def deposit(self,amount:int):
-#         self.amount=amount
-#         print(f"Current Balance after deposit:{self.Balance+self.amount}")
+#Create a BankAccount with deposit and withdraw methods
+class BankAccount:
+    Balance=100000
+    def deposit(self,amount:int):
+        self.amount=amount
+        print(f"Current Balance after deposit:{self.Balance+self.amount}")
 
-#     def withdraw(self,amount:int):
-#         self.amount=amount
-#         print(f"Available balance after withdraw :{self.Balance-self.amount}")
+    def withdraw(self,amount:int):
+        self.amount=amount
+        print(f"Available balance after withdraw :{self.Balance-self.amount}")
 
-# B1=BankAccount()
-# B1.deposit(20000)
-# B1.withdraw(15000)
+B1=BankAccount()
+B1.deposit(20000)
+B1.withdraw(15000)
 
-# #Create a ShoppingCart class with add_item() and remove_item().
-# class ShoppingCart:
-#     def __init__(self):
-#         self.items=[]
+#Create a ShoppingCart class with add_item() and remove_item().
+class ShoppingCart:
+    def __init__(self):
+        self.items=[]
 
-#     def add_item(self,*products):
-#         self.items.extend(products)
-#         print(self.items)
+    def add_item(self,*products):
+        self.items.extend(products)
+        print(self.items)
 
-#     def remove_item(self,product):
-#         self.items.remove(product)
-#         print(self.items)
+    def remove_item(self,product):
+        self.items.remove(product)
+        print(self.items)
 
-# S1=ShoppingCart()
-# S1.add_item("Laptop","Mobile","Charger","Keyboard","Mouse","Adapter")
-# S1.remove_item("Laptop")
+S1=ShoppingCart()
+S1.add_item("Laptop","Mobile","Charger","Keyboard","Mouse","Adapter")
+S1.remove_item("Laptop")
 
-# #Create a LibraryBook class with borrow() and return_book().
-# class LibraryBook:
-#     def __init__(self,title:str,author:str,is_barrowed:bool=False):
-#         self.title=title
-#         self.author=author
-#         self.is_barrowed=is_barrowed
+#Create a LibraryBook class with borrow() and return_book().
+class LibraryBook:
+    def __init__(self,title:str,author:str,is_barrowed:bool=False):
+        self.title=title
+        self.author=author
+        self.is_barrowed=is_barrowed
 
-#     def barrowed(self):
-#         if self.is_barrowed == True:
-#             print("Aleady Barrowed")
-#         else:
-#             self.is_barrowed=True
-#             print("Successful")
+    def barrowed(self):
+        if self.is_barrowed == True:
+            print("Aleady Barrowed")
+        else:
+            self.is_barrowed=True
+            print("Successful")
 
-#     def return_book(self):
-#         if self.is_barrowed==True:
-#             self.is_barrowed=False
-#             print("Book has returned succefully")
-#         else:
-#             print("Not Barrowed")
+    def return_book(self):
+        if self.is_barrowed==True:
+            self.is_barrowed=False
+            print("Book has returned succefully")
+        else:
+            print("Not Barrowed")
 
-# B1=LibraryBook("Python","Zahid",False)
-# B1.barrowed()
-# B1.return_book()
+B1=LibraryBook("Python","Zahid",False)
+B1.barrowed()
+B1.return_book()
 
-# #Create a VotingSystem class that checks whether a person can vote.
-# class VotingSystem:
-#     def __init__(self,age:int):
-#         self.age=age
+#Create a VotingSystem class that checks whether a person can vote.
+class VotingSystem:
+    def __init__(self,age:int):
+        self.age=age
 
-#     def Vote(self):
-#         if self.age>=18:
-#             print("Valid to vote")
-#         else:
-#             print("Your are not 18+")
+    def Vote(self):
+        if self.age>=18:
+            print("Valid to vote")
+        else:
+            print("Your are not 18+")
 
-# V1=VotingSystem(29)
-# V1.Vote()
+V1=VotingSystem(29)
+V1.Vote()
 
-# #Create a Result class that calculates pass/fail.
-# class Result:
-#     def __init__(self,student_name:str,marks:list[int]):
-#         self.student_name=student_name
-#         self.marks=marks
+#Create a Result class that calculates pass/fail.
+class Result:
+    def __init__(self,student_name:str,marks:list[int]):
+        self.student_name=student_name
+        self.marks=marks
 
-#     def check_result(self):
-#         total=0
-#         for num in self.marks:
-#             total=total+num
-#         print(total)
-#         if total>=35:
-#             print("Pass")
-#         else:
-#             print("Fail")
+    def check_result(self):
+        total=0
+        for num in self.marks:
+            total=total+num
+        print(total)
+        if total>=35:
+            print("Pass")
+        else:
+            print("Fail")
 
-# S1=Result("Zahid",[9,3,6,7])
-# S1.check_result()
+S1=Result("Zahid",[9,3,6,7])
+S1.check_result()
 
-# #Create a Login class that validates username and password.
-# class Login:
-#     username="Zahid"
-#     password=2223
-#     def __init__(self,uname,passw):
-#         self.uname=uname
-#         self.passw=passw
+#Create a Login class that validates username and password.
+class Login:
+    username="Zahid"
+    password=2223
+    def __init__(self,uname,passw):
+        self.uname=uname
+        self.passw=passw
 
-#     def verify(self):
-#         if self.uname==self.username and self.passw==self.password:
-#             print("Login Successful!")
-#         else:
-#             print("Invalid Username or Password")
+    def verify(self):
+        if self.uname==self.username and self.passw==self.password:
+            print("Login Successful!")
+        else:
+            print("Invalid Username or Password")
 
-# U1=Login("Zahid",2223)
-# U1.verify()
+U1=Login("Zahid",2223)
+U1.verify()
 
-# #Create a HotelRoom class with booking and cancellation methods.
-# class HotelRoom:
+#Create a HotelRoom class with booking and cancellation methods.
+class HotelRoom:
    
-#     def __init__(self, Room_No=205,Room_Type="single",Price=3000,is_booked:bool=False):
-#         self.Room_No=Room_No
-#         self.Room_Type=Room_Type
-#         self.Price=Price
-#         self.is_booked=is_booked
+    def __init__(self, Room_No=205,Room_Type="single",Price=3000,is_booked:bool=False):
+        self.Room_No=Room_No
+        self.Room_Type=Room_Type
+        self.Price=Price
+        self.is_booked=is_booked
 
-#     def booking(self):
-#         if self.is_booked==False:
-#             print("Room is Available")
-#             self.is_booked=True
-#         else:
-#             print("Room is already booked")
+    def booking(self):
+        if self.is_booked==False:
+            print("Room is Available")
+            self.is_booked=True
+        else:
+            print("Room is already booked")
 
-#     def cancellation(self):
-#         if self.is_booked==True:
-#             print("Cancalation Successful!")
-#             self.is_booked=False
-#         else:
-#             print("Room is not booked")
+    def cancellation(self):
+        if self.is_booked==True:
+            print("Cancalation Successful!")
+            self.is_booked=False
+        else:
+            print("Room is not booked")
 
-# R1=HotelRoom()
-# R1.booking()
-# R1.cancellation()
+R1=HotelRoom()
+R1.booking()
+R1.cancellation()
